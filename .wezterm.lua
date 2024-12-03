dcbdb4410359ab8583d7639ac91669976226228a
Yajana N Rao <yajananrao@gmail.com>
2024-12-03 23:54:27 +0530

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,139 +3,126 @@ local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local act = wezterm.action
-
-local icons = {
-	["C:\\WINDOWS\\system32\\cmd.exe"] = wezterm.nerdfonts.md_console_line,
-	["bash"] = wezterm.nerdfonts.cod_terminal_bash,
-	["curl"] = wezterm.nerdfonts.mdi_flattr,
-	["gh"] = wezterm.nerdfonts.dev_github_badge,
-	["git"] = wezterm.nerdfonts.md_git,
-	["htop"] = wezterm.nerdfonts.md_chart_areaspline,
-	["btop"] = wezterm.nerdfonts.md_chart_areaspline,
-	["lua"] = wezterm.nerdfonts.seti_lua,
-	["node"] = wezterm.nerdfonts.dev_nodejs,
-	["npm"] = wezterm.nerdfonts.dev_npm,
-	["nvim-qt"] = wezterm.nerdfonts.custom_vim,
-	["nvim"] = wezterm.nerdfonts.custom_neovim,
-	["pacman"] = "󰮯 ",
-	["paru"] = "󰮯 ",
-	["pwsh.exe"] = wezterm.nerdfonts.md_console,
-	["C:\\Program Files\\PowerShell"] = wezterm.nerdfonts.md_console,
-	["sudo"] = wezterm.nerdfonts.fa_hashtag,
-	["vim"] = wezterm.nerdfonts.dev_vim,
-	["wget"] = wezterm.nerdfonts.mdi_arrow_down_box,
-	["zsh"] = wezterm.nerdfonts.dev_terminal,
-	["lazygit"] = wezterm.nerdfonts.cod_github,
-	["esbuild.exe"] = wezterm.nerdfonts.md_webpack,
-}
-
-local arrow_solid = ""
-local arrow_thin = ""
+local mux = wezterm.mux
 
 local config = {
-	font = wezterm.font("JetBrains Mono"),
-	enable_tab_bar = true,
-	hide_tab_bar_if_only_one_tab = true,
-	use_fancy_tab_bar = false,
-	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
-	keys = {
-		-- tabs
-		{
-			key = "f",
-			mods = "LEADER",
-			action = act.ShowTabNavigator,
-		},
-		{
-			key = "i",
-			mods = "LEADER",
-			action = act.ActivatePaneDirection("Prev"),
-		},
-		{
-			key = "o",
-			mods = "LEADER",
-			action = act.ActivatePaneDirection("Next"),
-		},
-		-- splitting
-		{
-			mods = "LEADER",
-			key = "v",
-			action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-		},
-		{
-			mods = "LEADER",
-			key = "s",
-			action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-		},
-		{
-			mods = "LEADER",
-			key = "m",
-			action = act.TogglePaneZoomState,
-		},
-		-- rotate panes
-		{
-			mods = "LEADER",
-			key = "Space",
-			action = act.RotatePanes("Clockwise"),
-		},
-		-- show the pane selection mode, but have it swap the active and selected panes
-		{
-			mods = "LEADER",
-			key = "0",
-			action = act.PaneSelect({
-				mode = "SwapWithActive",
-			}),
-		},
-		-- Switch between workspaces
-		{
-			mods = "LEADER",
-			key = "w",
-			action = act.ShowLauncherArgs({
-				flags = "FUZZY|WORKSPACES",
-			}),
-		},
-		{ key = "n", mods = "LEADER", action = act.SwitchWorkspaceRelative(1) },
-		--Rename Workspace
-		{
-			mods = "LEADER",
-			key = "R",
-			action = act.PromptInputLine({
-				description = "Enter new name for workspace",
-				action = wezterm.action_callback(function(_, _, line)
-					if line then
-						wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
-					end
-				end),
-			}),
-		},
-		{ key = "p", mods = "LEADER", action = wezterm.action.SwitchWorkspaceRelative(-1) },
-	},
-	tab_bar_at_bottom = false,
-	tab_max_width = 30,
-	window_close_confirmation = "NeverPrompt",
-	window_decorations = "RESIZE",
-	cursor_thickness = 4,
-
 	webgpu_power_preference = "HighPerformance",
-	cursor_blink_ease_in = "Constant",
-	cursor_blink_ease_out = "Constant",
-	-- for transparent background
-	window_background_opacity = 0.98,
-	window_frame = {
-		border_left_width = "0.0cell",
-		border_right_width = "0.0cell",
-		border_bottom_height = "0.10cell",
-		border_top_height = "0.0cell",
-	},
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
+
 	inactive_pane_hsb = {
 		brightness = 0.5,
 		saturation = 0.5,
 	},
+}
+
+-- Cursor
+config.cursor_thickness = 4
+config.default_cursor_style = "BlinkingBar"
+config.force_reverse_video_cursor = true
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+
+-- fonts
+config.font = wezterm.font("JetBrains Mono")
+-- config.font_size = 11
+-- config.line_height = 1.35
+
+-- tabs
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.show_tab_index_in_tab_bar = true
+config.tab_bar_at_bottom = false
+config.tab_max_width = 64
+
+-- window
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "RESIZE"
+
+-- for transparent background
+config.window_background_opacity = 0.98
+
+config.window_frame = {
+	border_bottom_height = "0.5cell",
+}
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = "0.2cell",
+	bottom = 0,
+}
+
+-- Keymaps
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	-- tabs
+	{
+		key = "f",
+		mods = "LEADER",
+		action = act.ShowTabNavigator,
+	},
+	{
+		key = "i",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Prev"),
+	},
+	{
+		key = "o",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Next"),
+	},
+	-- splitting
+	{
+		mods = "LEADER",
+		key = "v",
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "s",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "m",
+		action = act.TogglePaneZoomState,
+	},
+	-- rotate panes
+	{
+		mods = "LEADER",
+		key = "Space",
+		action = act.RotatePanes("Clockwise"),
+	},
+	-- show the pane selection mode, but have it swap the active and selected panes
+	{
+		mods = "LEADER",
+		key = "0",
+		action = act.PaneSelect({
+			mode = "SwapWithActive",
+		}),
+	},
+	-- Switch between workspaces
+	{
+		mods = "LEADER",
+		key = "w",
+		action = act.ShowLauncherArgs({
+			flags = "FUZZY|WORKSPACES",
+		}),
+	},
+	{ key = "n", mods = "LEADER", action = act.SwitchWorkspaceRelative(1) },
+	--Rename Workspace
+	{
+		mods = "LEADER",
+		key = "R",
+		action = act.PromptInputLine({
+			description = "Enter new name for workspace",
+			action = wezterm.action_callback(function(_, _, line)
+				if line then
+					wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
+				end
+			end),
+		}),
+	},
+	{ key = "p", mods = "LEADER", action = wezterm.action.SwitchWorkspaceRelative(-1) },
 }
 
 -- Colorscheme
@@ -152,70 +139,82 @@ if wezterm.target_triple:find("windows") then
 	config.default_prog = { "pwsh" }
 end
 
-function title(tab, max_width)
-	local title = (tab.tab_title and #tab.tab_title > 0) and tab.tab_title or tab.active_pane.title
-	local process, other = title:match("^(%S+)%s*%-?%s*%s*(.*)$")
-	if icons[process] then
-		title = icons[process] .. " " .. (other or "")
-	end
+wezterm.on("gui-startup", function(window)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 
-	local is_zoomed = false
-	for _, pane in ipairs(tab.panes) do
-		if pane.is_zoomed then
-			is_zoomed = true
-			break
-		end
+local function tab_title(tab_info)
+	local title = tab_info.tab_title
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
 	end
-	if is_zoomed then -- or (#tab.panes > 1 and not tab.is_active) then
-		title = " " .. title
-	end
-
-	title = wezterm.truncate_right(title, max_width - 3)
-	return " " .. title .. " "
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
 end
 
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local title = title(tab, max_width)
+wezterm.on("format-tab-title", function(tab, tabs, panes, conf, hover, max_width)
+	local colors = conf.resolved_palette
+
+	local background = colors.tab_bar.inactive_tab.bg_color
+	local foreground = colors.tab_bar.inactive_tab.fg_color
+	local edge_background = colors.tab_bar.background
+
+	if tab.is_active or hover then
+		background = colors.tab_bar.active_tab.bg_color
+		foreground = colors.tab_bar.active_tab.fg_color
+	end
+
+	local edge_foreground = background
+
+	local title = tab_title(tab)
+
+	-- ensure that the titles fit in the available space,
+	-- and that we have room for the edges.
+	local max = max_width - 8
+	if #title > max then
+		title = wezterm.truncate_right(title, max) .. "…"
+	end
+
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = "" },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Attribute = { Intensity = tab.is_active and "Bold" or "Normal" } },
+		{ Text = " " .. (tab.tab_index + 1) .. ": " .. title .. " " },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = " " },
+	}
+end)
+
+wezterm.on("update-status", function(window, pane)
+	local time = wezterm.strftime("%Y-%m-%d %H:%M")
 	local colors = config.resolved_palette
-	local active_bg = colors.tab_bar.active_tab.bg_color
-	local inactive_bg = colors.tab_bar.inactive_tab.bg_color
 
-	local tab_idx = 1
-	for i, t in ipairs(tabs) do
-		if t.tab_id == tab.tab_id then
-			tab_idx = i
-			break
-		end
-	end
-	local is_last = tab_idx == #tabs
-	local next_tab = tabs[tab_idx + 1]
-	local next_is_active = next_tab and next_tab.is_active
-	local arrow = (tab.is_active or is_last or next_is_active) and arrow_solid or arrow_thin
-	local arrow_bg = inactive_bg
-	local arrow_fg = colors.tab_bar.inactive_tab_edge
+	-- Right status
+	window:set_right_status(wezterm.format({
+		-- Wezterm has a built-in nerd fonts
+		-- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
 
-	if is_last then
-		arrow_fg = tab.is_active and active_bg or inactive_bg
-		arrow_bg = colors.tab_bar.background
-	elseif tab.is_active then
-		arrow_bg = inactive_bg
-		arrow_fg = active_bg
-	elseif next_is_active then
-		arrow_bg = active_bg
-		arrow_fg = inactive_bg
-	end
-
-	local ret = tab.is_active
-			and {
-				{ Attribute = { Intensity = "Bold" } },
-				{ Attribute = { Italic = true } },
-			}
-		or {}
-	ret[#ret + 1] = { Text = title }
-	ret[#ret + 1] = { Foreground = { Color = arrow_fg } }
-	ret[#ret + 1] = { Background = { Color = arrow_bg } }
-	ret[#ret + 1] = { Text = arrow }
-	return ret
+		{ Text = " " },
+		{ Background = { Color = colors.background } },
+		{ Foreground = { Color = colors.ansi[8] } },
+		{ Text = wezterm.nerdfonts.ple_left_half_circle_thick },
+		{ Background = { Color = colors.ansi[8] } },
+		{ Foreground = { Color = colors.background } },
+		{ Text = wezterm.nerdfonts.md_calendar_clock .. " " },
+		{ Background = { Color = colors.ansi[1] } },
+		{ Foreground = { Color = colors.foreground } },
+		{ Text = " " .. time },
+		{ Background = { Color = colors.background } },
+		{ Foreground = { Color = colors.ansi[1] } },
+		{ Text = wezterm.nerdfonts.ple_right_half_circle_thick },
+	}))
 end)
 
 return config
